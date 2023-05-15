70f7541ee6684064ae4f991428463652e1bfb28c
yakicool <yaki.cool@gmail.com>
2023-05-15 03:09:57 +0000

    初始化wagtail

---
+++ fengyuanweb/fengyuanweb/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-v^d$2zj1qj*h#5#sr3cmmc#7vo_vq4-jrt_xp22oa19%js+%=)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
